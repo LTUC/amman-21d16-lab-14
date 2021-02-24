1a450904be6c3a0c428db74cc1b92801b4c5959b
Roya982 <roayarana@gmail.com>
2021-02-24 20:59:36 +0200

    bla

--- js/cart.js
+++ js/cart.js
@@ -9,8 +9,7 @@ let cart;
 function loadCart() {
   const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
   cart = new Cart(cartItems);
-  // console.log(cartItems);
-  // console.log(cart);
+  
 }
 
 // Make magic happen --- re-pull the Cart, clear out the screen and re-draw it
